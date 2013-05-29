73b481a061683b692ee4893ff55833d872e5722d
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-05-29 16:37:17 +0930

    CoderetreatsController wraps collection of running into a presenter

--- app/controllers/coderetreats_controller.rb
+++ app/controllers/coderetreats_controller.rb
@@ -2,17 +2,24 @@ Coderetreat = Struct.new(:status, :location)
 
 class CoderetreatsController < ApplicationController
   def running_today
-    @coderetreats = Object.new
-    def @coderetreats.not_started(&block)
-      [
-        Coderetreat.new('not_started', 'Chicago'),
-        Coderetreat.new('not_started', 'Seattle'),
-      ].each(&block)
-    end
-    def @coderetreats.in_session(&block)
-      [
-        Coderetreat.new('in_session', 'Berlin')
-      ].each(&block)
-    end
+    # @coderetreats = Object.new
+    # def @coderetreats.not_started(&block)
+    #   [
+    #     Coderetreat.new('not_started', 'Chicago'),
+    #     Coderetreat.new('not_started', 'Seattle')
+    #   ].each(&block)
+    # end
+    # def @coderetreats.in_session(&block)
+    #   [
+    #     Coderetreat.new('in_session', 'Berlin')
+    #   ].each(&block)
+    # end
+    coderetreats = [
+      Coderetreat.new('not_started', 'Chicago'),
+      Coderetreat.new('not_started', 'Seattle'),
+      Coderetreat.new('in_session', 'Berlin')
+    ]
+    @coderetreats =
+      CoderetreatLive::Coderetreats::Presenters::Collection.for(coderetreats)
   end
 end
---
+++ spec/controllers/coderetreats_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe CoderetreatsController do
+  describe "GET /running_today" do
+    it "uses a coderetreats collection presenter" do
+      coderetreats_presenter = double
+      stub_const("CoderetreatLive::Coderetreats::Presenters::Collection",
+        stub(for: coderetreats_presenter))
+      get :running_today
+      expect(assigns(:coderetreats)).to be(coderetreats_presenter)
+    end
+  end
+end
