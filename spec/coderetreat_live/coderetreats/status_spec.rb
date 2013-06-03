1f776d46d0b62ea2d52ec4a7abe97f32420a19d9
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-06-03 12:32:16 +0930

    Status service object built for updating coderetreat's status

---
+++ spec/coderetreat_live/coderetreats/status_spec.rb
@@ -0,0 +1,12 @@
+require 'coderetreats/status'
+
+describe CoderetreatLive::Coderetreats::Status do
+  describe ".update_to" do
+    let(:coderetreat) { double }
+    before { stub_const("::Coderetreat", double(find: coderetreat)) }
+    it "tells the coderetreat to update its status" do
+      coderetreat.should_receive(:update_status).with("in_session")
+      CoderetreatLive::Coderetreats::Status.update_to("5", "in_session")
+    end
+  end
+end
