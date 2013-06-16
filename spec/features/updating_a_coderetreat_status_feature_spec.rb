03d45f9565d7932c5fe510c79345618985bb1028
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-06-16 17:18:32 +1000

    Add extended scenario explanations

--- spec/features/updating_a_coderetreat_status_feature_spec.rb
+++ spec/features/updating_a_coderetreat_status_feature_spec.rb
@@ -8,7 +8,12 @@ feature 'Updating the status of a session' do
     Coderetreat.create!(status: 'in_session', location: 'Chicago')
   end
 
-  scenario 'starting a session' do
+  scenario 'starting a session', %q(
+    Given a coderetreat that has not started
+    When I start the coderetreat
+    And I go to the running coderetreats display page
+    Then I should see that the coderetreat is in session) do
+
     visit edit_coderetreat_status_url(not_started_coderetreat)
     click_button 'Start Session'
     visit running_today_coderetreats_url
@@ -17,7 +22,12 @@ feature 'Updating the status of a session' do
         text: not_started_coderetreat.location)
   end
 
-  scenario 'starting a break' do
+  scenario 'starting a break', %q(
+    Given a coderetreat that is in session
+    When I start a break for the coderetreat
+    And I go to the running coderetreats display page
+    Then I should see that the coderetreat is on break) do
+
     visit edit_coderetreat_status_url(in_session_coderetreat)
     click_button 'Start Break'
     visit running_today_coderetreats_url
--- spec/features/viewing_running_coderetreats_feature_spec.rb
+++ spec/features/viewing_running_coderetreats_feature_spec.rb
@@ -11,12 +11,20 @@ feature 'Viewing the coderetreats running today' do
 
   background { visit running_today_coderetreats_url }
 
-  scenario 'No coderetreats running today' do
+  scenario 'No coderetreats running today', %q(
+    Given there are no coderetreats running today
+    When I go to the running coderetreats display page
+    Then I should see that there are no coderetreats running) do
+
     Coderetreat.destroy_all
     expect(page).to have_content('There are no coderetreats running today')
   end
 
-  scenario 'Some coderetreats are running today' do
+  scenario 'Some coderetreats are running today', %q(
+    Given there are some coderetreats running today
+    When I go to the running coderetreats display page
+    Then I should see the running coderetreats grouped by status) do
+
     coderetreats.each do |coderetreat|
       within(".#{coderetreat.status}") do
         expect(page).to have_css('.coderetreat', text: coderetreat.location)
